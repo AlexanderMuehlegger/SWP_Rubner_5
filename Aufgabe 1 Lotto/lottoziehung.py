603d7fa45b2c07c4c00ae8bc9afdfda5bbcb1dd7
AlexanderMuehlegger <alexmuehlegger2000@gmail.com>
2022-09-28 13:23:40 +0200

    aufgabe 1

---
+++ Aufgabe 1 Lotto/lottoziehung.py
@@ -0,0 +1,48 @@
+import random
+
+zahlen_pool = []
+zahlen_pool_used = []
+ziehungen = []
+
+statistik_dic = {
+
+}
+
+MAX_ZAHL = 45
+
+for x in range(1, MAX_ZAHL+1):
+    zahlen_pool.append(x)
+
+
+def lotto_ziehung(pool):
+    if len(pool) <= 0:
+        print("finished")
+        return False
+    
+    index = random.randint(0, len(pool)-1)
+    ziehung = pool[index]
+
+    pool.pop(index)
+    ziehungen.append(ziehung)
+
+def statistik(ziehung):
+    global statistik_dic
+    print(ziehung)
+    for x in ziehung:
+        statistik_dic[x] = statistik_dic.get(x, 0) + 1
+
+def print_statistik():
+    global statistik_dic
+    for x in range(1, MAX_ZAHL+1):
+        print("{}: ".format(x) +  str(statistik_dic[x]))
+
+zahlen_pool_used = zahlen_pool.copy()
+
+for x in range(1000):
+    for y in range(6):
+        lotto_ziehung(zahlen_pool_used)
+    statistik(ziehungen)
+    ziehungen = []
+    zahlen_pool_used = zahlen_pool.copy()
+
+print_statistik()
